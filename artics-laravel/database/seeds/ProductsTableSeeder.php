8f4e37e67bedba7912d038561ab57a75f67daefd
Olivia Pradervand <olivia.pradervand@heig-vd.ch>
2020-06-22 13:43:01 +0200

    BD test
    creation d'une table de migration products, du modele lié ainsi qu 'un seeder

---
+++ artics-laravel/database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class ProductsTableSeeder extends Seeder
+{
+
+    private function randPicture() {
+        $nbPicture = rand(1,4);
+        $picture = 'product' . $nbPicture . '.png';
+        return $picture;
+    }
+
+    private function randDate() {
+        $nbJours = rand(-2800,0);
+        return Carbon::now()->addDays($nbJours);
+    }
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->delete();
+        for ($i = 1; $i <= 10; $i++) {
+            $date=$this->randDate();
+            DB::table('products')->insert([
+                'shortDescr' => 'Model' . $i,
+                'longDescr' => 'Descr' . $i,
+                'distinctiveSign' => 'Sign' . $i,
+                'picture' => $this->randPicture(),
+                'brand_id'=>rand(1,3),
+                'created_at'=>$date,
+                'updated_at'=>$date
+            ]);
+        }
+    }
+}
