32d29dceb59e6e384a0d99e6e4d5f09483fda88c
Stephane-panda <stephane.bondabelo@heig-vd.ch>
2020-06-30 11:54:35 +0200

    Update BikesTableSeeder.php

--- artics-laravel/database/seeds/BikesTableSeeder.php
+++ artics-laravel/database/seeds/BikesTableSeeder.php
@@ -39,10 +39,10 @@ class BikesTableSeeder extends Seeder
     }
 
     private function randCategory() {
-        $cat = ["VTT", "Gravel", "e-VTT", "Road", "e-Road", "Junior"];
+        $cat = ["Mountain", "Gravel", "e-Mountain", "Road", "e-Road"];
         return $cat[rand(0,count($cat)-1)];
     }
-
+    
     /**
      * Run the database seeds.
      *
