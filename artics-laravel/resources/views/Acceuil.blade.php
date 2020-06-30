51f6de3476a88b50d502dd7aad93feecc79597e9
Julie Greset <julie.greset@heig-vd.ch>
2020-06-30 15:00:56 +0200

    Update fonts

--- artics-laravel/resources/views/Acceuil.blade.php
+++ artics-laravel/resources/views/Acceuil.blade.php
@@ -5,13 +5,11 @@
     <div class="row accueil" >
       <div class="col-lg-12 text-center">
         <img class="img-fluid" src="" alt="">
-        <h1 class="mt-5">Au Bike Test Gryon, tu peux essayer tous les vélos que tu veux pour seulement CHF 20.- /jour</h1>
+        <h1 class="mt-5">Au Bike Test Gryon, tu peux essayer <br> tous les vélos que tu veux pour <br> seulement CHF 20.- /jour</h1>
         <!-- <h2 class="mt-5">2-4 octobre 2020</h2>
         <p class="mt-5" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh vitae quis auctor nulla. Praesent tempor, aliquet leo orci a tellus enim turpis suscipit.</p> -->
-        <a type="button" class="btn btn-primary btn-lg mt-5" href="/Inscription">Réserver un billet</a>
+        <a type="button" class="btn btn-primary btn-lg mt-5" href="/Inscriptions">Réserver un billet</a>
       </div>
     </div>
-
-
 
 @endsection
