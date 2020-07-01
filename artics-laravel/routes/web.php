cf359365750f717902108b13a426366e6b21d746
Julie Greset <julie.greset@heig-vd.ch>
2020-07-01 10:48:18 +0200

    try

--- artics-laravel/routes/web.php
+++ artics-laravel/routes/web.php
@@ -104,3 +104,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
