bebaac8d3e9a4e5ffbbb0bdf9474a1eb400da88d
Olivia Pradervand <olivia.pradervand@heig-vd.ch>
2020-07-01 16:34:50 +0200

    Page Tests

--- Frontend/src/js/test.js
+++ Frontend/src/js/test.js
@@ -1,2 +1,27 @@
 /* import Reviews from 'App/entities/test/collection.js';  
  import Reviewscollection from 'App/entities/test/viewCollection';   */
+
+import Tests from 'App/entities/Test/collection.js';
+import Testscollection from 'App/entities/Test/viewCollection'; 
+
+let tests = new Tests();
+
+let vtests= new Testscollection({
+    collection : tests,
+    el: '#tests'
+}); 
+
+tests.fetch();
+
+// //New test modal
+// $('#addTestModal').modal({
+//     show: true
+// })
+// $('#startTest').modal({
+//     show: false
+// })
+
+
+
+
+
