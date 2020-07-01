d94ccfcf63b9ba687a081493f93ac17cd93c2411
Julie Greset <julie.greset@heig-vd.ch>
2020-07-01 12:02:39 +0200

    debug julie

--- artics-laravel/routes/web.php
+++ artics-laravel/routes/web.php
@@ -97,7 +97,7 @@ Route::get('AddProduct', 'ProductController@create');
 
 /* Route::get('import_data', 'ProductController@uploadFile'); */
 
-Auth::routes();
+// Auth::routes();
 
- Route::get('/home', 'HomeController@index')->name('home');
+//  Route::get('/home', 'HomeController@index')->name('home');
 
