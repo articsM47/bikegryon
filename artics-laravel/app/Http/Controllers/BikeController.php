dfdf2c582ba1adbf895f0eb155bb3ebd8277ca39
Michelle Ponti <michelle.ponti@heig-vd.ch>
2020-07-01 14:02:51 +0200

    Product page images

--- artics-laravel/app/Http/Controllers/BikeController.php
+++ artics-laravel/app/Http/Controllers/BikeController.php
@@ -58,6 +58,7 @@ public function affiche() {
     }
 
     public function afficheproduit($Bike) {
+      
       $bike = Bike::findOrFail($Bike);
       return view('Product', compact ('bike'));
       //return view('Product')->with('shortDescr', 'Victoria');
--- artics-laravel/resources/views/product.blade.php
+++ artics-laravel/resources/views/product.blade.php
@@ -7,9 +7,10 @@
 
 
     <ul class="breadcrumb">
-      <button href="/Catalogue" ><a href="/Catalogue"><img src="http://127.0.0.1:8000/fonts/arrow_back.svg"></a></button>
-      <li><a href="/Catalogue">Les vélos</a> <span class="divider"></span></li>
-      <li class="active">Le Produit</li>
+      <button href="/Catalogue" ><a href="/Catalogue"><img src="http://127.0.0.1:8000/fonts/arrow_back.svg" alt="arrow_back"></a></button>
+      <li><a href="/Catalogue">Catalogue</a> <span class="divider"></span></li>
+      <li>{{$bike -> category}}<span class="divider"></span></li>
+      <li class="active">{{$bike -> shortDescr}}</li>
     </ul>
 
 
@@ -22,6 +23,25 @@
           <p class="card-text">Parfait pour la montagne</p>
           <p class="card-text">{{$bike -> frameSize}} {{$bike -> frameUnit}}</p>
           <p class="card-text">{{$bike -> category}}</p>
+
+          @if ({{$bike -> category}} === "Mountain"||{{$bike -> category}} === "e_Mountain"){
+            <img src="http://127.0.0.1:8000/fonts/mountain.svg" alt="mountains_icon" width = 100%>
+          }
+          
+          @elseif ({{$bike -> category}} === "Road"||{{$bike -> category}} === "e_Road"){
+            <img src="http://127.0.0.1:8000/fonts/street.svg" alt="street_icon" width = 100%>
+          }
+
+          @elseif ({{$bike -> category}} === "Road"||{{$bike -> category}} === "e_Road"){
+            <img src="http://127.0.0.1:8000/fonts/street.svg" alt="street_icon" width = 100%>
+          }
+
+          @else{
+  
+          }
+
+          @endif
+  
           <img src="http://127.0.0.1:8000/pictures/brands/brand1.png" alt="..." width = 100%>
         </div>
       </div>
