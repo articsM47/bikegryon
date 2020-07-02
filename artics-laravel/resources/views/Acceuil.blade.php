fe50ca54e64f9b04067f2a8162176e7cd1a5189b
Julie Greset <julie.greset@heig-vd.ch>
2020-07-02 09:20:45 +0200

    Update home button

--- artics-laravel/resources/views/Acceuil.blade.php
+++ artics-laravel/resources/views/Acceuil.blade.php
@@ -8,7 +8,7 @@
       <h2 class="mt-5">Au Bike Test Gryon, tu peux essayer <br> tous les vélos que tu veux pour <br> seulement CHF 20.- /jour</h1>
       <!-- <h2 class="mt-5">2-4 octobre 2020</h2>
         <p class="mt-5" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh vitae quis auctor nulla. Praesent tempor, aliquet leo orci a tellus enim turpis suscipit.</p> -->
-      <a type="button" class="button" href="/Inscriptions"><span>Réserver un billet</span></a>
+      <a type="button" class="button button-home" href="/Inscriptions"><span>Réserver un billet</span></a>
     </div>
   </div>
 
