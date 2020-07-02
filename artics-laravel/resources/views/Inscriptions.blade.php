bd8350c5266dc7ddee34e9d4346cd28bb24a7ac6
Ingrid Sorg <ingrid.sorgpacheco@heig-vd.ch>
2020-07-02 16:13:35 +0200

    CSS-Creation

--- artics-laravel/resources/views/Inscriptions.blade.php
+++ artics-laravel/resources/views/Inscriptions.blade.php
@@ -5,9 +5,9 @@
   <div class="row">
     <div class="col-lg-12 ">
       <h1 class="mt-5">Réserve ton billet</h1>
-      <h4 class="mt-5">Vendredi 2, samedi 3 et dimanche 4 octobre 2020</h4>
+      <h4 id="program-text" class="mt-5">Vendredi 2, samedi 3 et dimanche 4 octobre 2020</h4>
     </div>
-    <div class="container">
+    <div class="container program-container">
       <div class="row">
         <div class="col-lg-4 ">
           <div class="card-deck">
@@ -152,15 +152,20 @@
           </label>
         </div>
       </div>
+      <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash trash-reservation" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+      </svg>
     </div>
   </div>
-  <div class="col-lg-12 text-center emailText">
+  <div class="col-lg-12 text-center email-text">
     <hr>
     </hr>
     <p class="mt-5">Il nous faut juste ton adresse e-mail pour
       confirmer ta réservation</p>
   </div>
 
+</div>
 </div>
 <div class="form-group text-center ">
   <input type="email" class="inputUser email" id="email" aria-describedby="emailHelp" placeholder="Email">
