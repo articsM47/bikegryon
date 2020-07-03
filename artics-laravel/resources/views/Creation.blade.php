76963138ed20393c6e34d64513e7b67e4d2c7b38
Ingrid Sorg <ingrid.sorgpacheco@heig-vd.ch>
2020-07-03 13:30:03 +0200

    CSS Créer un compte

--- artics-laravel/resources/views/Creation.blade.php
+++ artics-laravel/resources/views/Creation.blade.php
@@ -6,53 +6,38 @@
         <div class="col-lg-12 text-center ">
             <h1 class="mt-5">Merci pour ta réservation ! </h1>
             <p class="mt-5 normal-text">Nous avons envoyé une confirmation à l’adresse : lucie.dupont@gmail.com</p>
-            <p class="mt-5 normal-text">Crée ton compte maintenant en choissant un mot de passe </p>
+            <p class="mb-5 normal-text">Crée ton compte maintenant en choissant un mot de passe </p>
         </div>
     </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 ">
-                <div class="card-group">
-                    <form method="POST" action="{{ url('Confirmation') }}" accept-charset="UTF-8">
-                        @csrf
-                        <div class="card user-info">
-                            <div class="card-body">
-                                <h5 class="card-title">Tes informations :</h5>
-                                <input class="form-control" type="text" name="fullname" value="{{$name ?? ''}} {{$firstname ?? ''}}" readonly>
-                                <input class="form-control" type="text" name="birthDay" value="{{$birthDay ?? ''}}" readonly>
-                                <input type="email" class="form-control d-none" name="email1" value="{{$email1 ?? ''}}">
-                                <input class="form-control d-none" type="text" name="name" value="{{$name ?? ''}}">
-                                <input class="form-control d-none" type="text" name="firstname" value="{{$firstname ?? ''}}">
-                                <a href="Login" class="badge badge-light">ce n'est pas mois</a>
-                            </div>
-                        </div>
+</div>
+<div class="container">
+    <div class="row ">
+        <div class="col-lg-6">
+            <form method="POST" action="{{ url('Confirmation') }}" accept-charset="UTF-8">
+                @csrf
+                <div class="align-content-*-end">
+                    <input class="new-user" type="text" name="fullname" value="{{$name ?? ''}} {{$firstname ?? ''}}" placeholder="name" readonly><br>
+                    <input class="new-user" type="text" name="birthDay" value="{{$birthDay ?? ''}}" readonly><br>
+                    <input class="new-user" type="email" name="email1" value="{{$email1 ?? ''}}"><br>
+                    <a href="Login" class="form-option-text">Ce n'est pas moi</a>
                 </div>
-            </div>
-            <div class="col-lg-6 ">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Tu n’as plus qu’à choisir un mot de passe :</h5>
-
-                        <label for="password" class="sr-only">Password</label>
-                        <input type="password" class="form-control" name="password" placeholder="Password">
-                        <label for="confrimpassword" class="sr-only">Password</label>
-                        <input type="password" class="form-control" name="confrimpassword" placeholder="confirme Password">
-                    </div>
+        </div>
+        <div class="col-lg-6 user-info">
+            <div>
+                <div>
+                    <label for="password" class="sr-only">Password</label>
+                    <input type="password" class="inputUser create" name="password" placeholder="Mot de passe"><br>
+                    <label for="confrimpassword" class="sr-only">Password</label>
+                    <input type="password" class="inputUser create" name="confrimpassword" placeholder="Confirmer le mot de passe">
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-
-<div class="form-group text-center ">
-    <input class="btn btn-primary btn-lg" type="submit" value="reserver !">
-    <a href="/Confirmation" type="button" class="badge badge-light">continuer sans compte</a>
 </div>
-<div class="form-group text-center ">
-    <a href="/Login" type="button" class="badge badge-light ">j'ai déjà un compte</a>
-    <input class="btn btn-primary btn-lg" type="submit" value="reserver !">
-    </form>
+<div class="text-center ">
+    <button id="submit-account" type="submit"><span>Continuer</span></button><br>
+    <a href="/Confirmation" class="form-option-text">Ne pas créer de compte</a>
 </div>
 </form>
 @endsection
