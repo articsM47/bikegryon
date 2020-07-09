4425f391f0808189a0b902a52682ff695a75bd40
Stephane-panda <stephane.bondabelo@heig-vd.ch>
2020-07-09 13:38:45 +0200

    test bug serveur

--- Frontend/src/App/entities/Test/viewCollection.js
+++ Frontend/src/App/entities/Test/viewCollection.js
@@ -91,7 +91,7 @@ export default class extends ImView {
         let csrfToken = $('#csrf-token').val();
         $.ajax({
             type: "POST",
-            url: baseURL + "/Test",
+            url: baseURL + "Test",
             data: {
                 _token: csrfToken,
                 badgeNo: inputClientNumber,
@@ -119,7 +119,7 @@ export default class extends ImView {
         let csrfToken = $('#csrf-token').val();
         $.ajax({
             type: "POST",
-            url: baseURL + "/Test/Review",
+            url: baseURL + "Test/Review",
             data: {
                 _token: csrfToken,
                 testId: testId,
