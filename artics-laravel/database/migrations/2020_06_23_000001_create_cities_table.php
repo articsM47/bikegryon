67272ed316bc2a4b2baef95bf2413ddd71e4176c
Stephane-panda <stephane.bondabelo@heig-vd.ch>
2020-07-09 11:42:38 +0200

    Update 2020_06_23_000001_create_cities_table.php

--- artics-laravel/database/migrations/2020_06_23_000001_create_cities_table.php
+++ artics-laravel/database/migrations/2020_06_23_000001_create_cities_table.php
@@ -15,7 +15,7 @@ class CreateCitiesTable extends Migration
     {
         Schema::create('cities', function (Blueprint $table) {
             $table->primary('id');
-            $table->bigIncrements('id')->unique();
+            $table->Increments('id')->unique();
             $table->integer('postalCode');//must be unique for a country
             $table->string('cityName');
             $table->string('canton',2)->nullable();
