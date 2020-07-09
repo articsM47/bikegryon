e2aeb1111be8bc5b959dea2ac090ed8514ed31f6
Stephane-panda <stephane.bondabelo@heig-vd.ch>
2020-07-09 15:34:56 +0200

    aled

--- Frontend/src/App/entities/person/viewCollection.js
+++ Frontend/src/App/entities/person/viewCollection.js
@@ -65,7 +65,7 @@ export default class extends ImView {
         let csrfToken = $('#csrf-token').val();
         $.ajax({
             type: "POST",
-            url: baseURL + "/Member/Badge",
+            url: baseURL + "Member/Badge",
             data: {
                 _token: csrfToken,
                 clientId: inputClientId,
